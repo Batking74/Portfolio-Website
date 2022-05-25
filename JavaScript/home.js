395acc9dad37393d1d0a922c4810f669b3acd971
Nazir <Batking74@gmail.com>
2022-05-25 02:40:28 -0400

    Not finnished, struggling to program a few things. Have to problem solve.

---
+++ JavaScript/home.js
@@ -0,0 +1,124 @@
+// Toggle Navbar
+const toggleBtn = document.querySelector('.toggle-btn');
+const linkContainer = document.querySelector('.links-container');
+toggleBtn.addEventListener('click', () => {
+    toggleBtn.classList.toggle('active');
+    linkContainer.classList.toggle('show');
+})
+
+// Navigation Link Toggle
+const links = document.querySelectorAll('.link');
+links.forEach(link => {
+    link.addEventListener('click', () => {
+        links.forEach(ele => ele.classList.remove('active'));
+        link.classList.add('active');
+    })
+})
+
+// Toggles
+const backgroundBtn = document.querySelector('#background-Toggle');
+const navigation = document.querySelector('.navbar')
+const footer = document.querySelector('#footerBackgroundColorToggle')
+const uniqueText = document.querySelectorAll("#highlight-Toggle");
+backgroundBtn.addEventListener('click', () => {
+    document.body.classList.toggle('changeBackgroundColor');
+    navigation.classList.toggle('changeNavColorByToggle');
+    footer.classList.toggle('footerBackgroundColorToggle');
+    uniqueText.classList.toggle('highlight-Toggle');
+})
+
+// Play Music
+const playMusic = document.querySelector('.playMusic');
+playMusic.addEventListener('click', () => {
+    playMusic.innerHTML = `<svg style="width: 20px; height: 20px" viewBox="0 0 24 24"><path fill="currentColor" d="M14,19H18V5H14M6,19H10V5H6V19Z" /></svg>`;
+})
+
+// Speechsynthesis
+const playButton = document.getElementById('play-button');
+const textInput = document.getElementById('ReadTextAI');
+playButton.addEventListener('click', () => {
+    playText(textInput.textContent);
+    playButton.innerHTML = `<svg style="width: 20px; height: 20px" viewBox="0 0 24 24"><path fill="currentColor" d="M14,19H18V5H14M6,19H10V5H6V19Z" /></svg>`;
+    playButton.addEventListener('click', () => {
+        speechSynthesis.cancel();
+        playButton.innerHTML = `Read Aloud`;
+        playButton.reset();
+    })
+})
+const playText = (text) => {
+    const utterance = new SpeechSynthesisUtterance(text)
+    utterance.rate = 1;
+    speechSynthesis.speak(utterance)
+    }
+
+// Filter Projects
+const filter = document.querySelectorAll('.filter-btn');
+filter.forEach(btn => {
+    btn.addEventListener('click', () => {
+        filter.forEach(ele => ele.classList.remove('active'));
+        btn.classList.add('active');
+    })
+})
+
+// Creating Dynamic Project Card
+const projectContainer = document.querySelector('.project-container');
+class myProjects {
+    constructor(name, tags, image) {
+        // New Instances
+        this.name = name;
+        this.tags = tags;
+        this.image = image;
+    }
+}
+// Instantiation
+item1 = new myProjects('Project one', '#Javascript', '/IMG/Lion2.JPG');
+item2 = new myProjects('Project one', '#Bootstrap', '/IMG/Lion2.JPG');
+item3 = new myProjects('Project one', '#Java', '/IMG/Lion2.JPG');
+item4 = new myProjects('Project one', '#React', '/IMG/Lion2.JPG');
+item5 = new myProjects('Project one', '#Python', '/IMG/Lion2.JPG');
+item6 = new myProjects('Project one', '#Javascript', '/IMG/Lion2.JPG');
+item7 = new myProjects('Project one', '#Javascript', '/IMG/Lion2.JPG');
+item8 = new myProjects('Project one', '#Javascript', '/IMG/Lion2.JPG');
+myProjectsArray = [item1, item2, item3, item4, item5, item6, item7, item8];
+myProjectsArray.forEach(project => {
+    projectContainer.innerHTML += `
+            <div class="project-card data-tags="#all, ${project.tags}">
+                <img src="${project.image}" alt="">
+                <div class="content">
+                    <h1 class="project-name">${project.name}</h1>
+                    <span class="tags">${project.tags}</span>
+                </div>
+            </div>
+    `;
+})
+
+// Contact Form
+const form = document.querySelector('#form');
+const contactBtn = document.querySelector('.contact-btn');
+const firstName = document.querySelector('.first-name');
+const lastName = document.querySelector('.last-name');
+const email = document.querySelector('.email');
+const msg = document.querySelector('.message');
+const formError = document.querySelectorAll('.validationError');
+
+const validateNames = () => {
+    if(firstName.value === '') {
+        formError.innerHTML = 'This is required!';
+    }
+};
+
+contactBtn.addEventListener('click', (stop) => {
+    stop.preventDefault();
+    validateNames();
+    // validateEmail();
+    // validateMSG();
+    form.reset();
+    }
+)
+
+const validateEmail = () => {
+
+};
+const validateMSG = () => {
+
+};
---
+++ JavaScript/server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const path = require('path');
+
+let initialPath = path.join(__dirname, "public");
+
+app.use(express.static(initialPath));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(initialPath, "index.html"));
+})
+
+app.listen('3000', () => {
+    console.log('listening.....');
+})
