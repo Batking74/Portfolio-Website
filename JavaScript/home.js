3e72719f01b0999948f71ac6956d6c724bcd9079
Nazir Knuckles <nazirknuckles@gmail.com>
2023-10-25 05:11:05 -0400

    Created a queue data structure and organized my code a little.

--- JavaScript/home.js
+++ JavaScript/home.js
@@ -1,21 +1,61 @@
-// Countdown
-  // Step 1. What element do we want to animate?
-  const countdown = document.querySelector("#countdown");
-  // Step 2. What function will change it each time?
+// Queue Data Structure
+class Queue {
+    constructor() {
+        this.list = [];
+    }
 
-  const countItDown = () => {
+    // O(1)
+    enqueue(data) {
+        this.list.push(data);
+    }
+    
+    // O(1)
+    dequeue() {
+        if(this.isEmpty()) return 'Queue is Empty!';
+        return this.list.shift();
+    }
+    
+    // O(1)
+    peek() {
+        if(this.isEmpty()) return 'Queue is Empty!';
+        return this.list[0];
+    }
+    
+    // O(1)
+    isEmpty() {
+        if(this.list.length == 0) return true;
+        return false;
+    }
+    
+    // O(1)
+    size() {
+        return this.list.length;
+    }
+    
+    // O(1)
+    print() {
+        console.log(this.list);
+    }
+    
+    // O(1)
+    getIndex(i) {
+        return this.list[i]
+    }
+}
+
+// Countdown
+const countdown = document.querySelector("#countdown");
+const countItDown = () => {
     const currentTime = parseFloat(countdown.textContent);
     if (currentTime > 0) {
-       countdown.textContent = currentTime - 1;   
+        countdown.textContent = currentTime - 1;   
     } else {
         window.clearInterval(timer);
         location.replace('https://www.instagram.com/mr_blacknificent/')
     }
-    
-  };
+};
 
-//   Step 3: Call that on an interval
-  var timer = window.setInterval(countItDown, 1000);
+const timer = window.setInterval(countItDown, 1000);
 
 // Toggle Navbar
 const toggleBtn = document.querySelector('.toggle-btn');
@@ -36,8 +76,8 @@ links.forEach(link => {
 
 // Toggles
 const backgroundBtn = document.querySelector('#background-Toggle');
-const navigation = document.querySelector('.navbar')
-const footer = document.querySelector('#footerBackgroundColorToggle')
+const navigation = document.querySelector('.navbar');
+const footer = document.querySelector('#footerBackgroundColorToggle');
 const uniqueText = document.querySelectorAll("#highlight-Toggle");
 backgroundBtn.addEventListener('click', () => {
     document.body.classList.toggle('changeBackgroundColor');
@@ -46,6 +86,7 @@ backgroundBtn.addEventListener('click', () => {
     uniqueText.classList.toggle('highlight-Toggle');
 })
 
+// Working on this feature
 // Play Music
 // const playMusic = document.querySelector('.playMusic');
 // playMusic.addEventListener('click', () => {
@@ -74,16 +115,8 @@ const playText = (text) => {
     const utterance = new SpeechSynthesisUtterance(text);
     utterance.rate = 1;
     speechSynthesis.speak(utterance);
-    }
+}
 
-// Filter Projects
-const filter = document.querySelectorAll('.filter-btn');
-filter.forEach(btn => {
-    btn.addEventListener('click', () => {
-        filter.forEach(ele => ele.classList.remove('active'));
-        btn.classList.add('active');
-    })
-})
 
 // Creating Dynamic Project Card
 const projectContainer = document.querySelector('.project-container');
@@ -96,31 +129,105 @@ class myProjects {
         this.link = link;
     }
 }
+
+const placeHolder = ['Coming Soon', '', './IMG/Lion2.JPG', '#']
+const tags = ['#HTML', '#CSS', '#JS', '#Node.js', '#SQL', '#Jquery', '#Bootstrap'];
+
+// Project Titles
+const title = new Queue();
+title.enqueue('Palmer Studios');
+title.enqueue('Budget Calculator');
+title.enqueue('Talking Computer');
+title.enqueue('Digital Art Portfolio');
+title.enqueue('Password Generator');
+title.enqueue('Meal Discovery');
+
+// Project Hashtags
+const hashTags = new Queue();
+hashTags.enqueue(tags[2]);
+hashTags.enqueue(`${tags[0]} ${tags[1]} ${tags[2]} ${tags[3]} ${tags[4]}`);
+hashTags.enqueue(`${tags[0]} ${tags[1]} ${tags[2]}`);
+hashTags.enqueue(`${tags[0]} ${tags[1]}`);
+hashTags.enqueue(`${tags[0]} ${tags[1]} ${tags[2]}`);
+hashTags.enqueue(`${tags[0]} ${tags[1]} ${tags[2]} ${tags[5]} ${tags[6]}`);
+
+// Project Images
+const image = new Queue();
+image.enqueue('./IMG/Palmer_Studios_Project.png');
+image.enqueue('./IMG/Budget_Calculator_Project.png');
+image.enqueue('./IMG/Talking_Computer_Project.png');
+image.enqueue('./IMG/Art_Portfolio_Project.png');
+image.enqueue('./IMG/Password Generator.png');
+image.enqueue('./IMG/Meal_Discovery_Project.png');
+
+// Project URL
+const url = new Queue();
+url.enqueue('https://batking74.github.io/Kekes_Model_Website/');
+url.enqueue('https://batking74.github.io/Budget-Calculator/');
+url.enqueue('https://batking74.github.io/Talking_Computer_Project/');
+url.enqueue('https://batking74.github.io/Digital_Art_Portfolio_Project--First-ever-project-/');
+url.enqueue('https://batking74.github.io/Password-Generator/');
+url.enqueue('https://mrwait-music.github.io/super-group-project/');
+url.enqueue(placeHolder[3]);
+url.enqueue(placeHolder[3]);
+projectsComingSoon(2);
+
+const productList = new Queue();
+
 // Instantiation
-item1 = new myProjects('Palmer Studios', '#Javascript', './IMG/Palmer_Studios_Project.png', 'https://batking74.github.io/Kekes_Model_Website/');
+for(let i = 0; i < title.size(); i++) {
+    const object = new myProjects(title.getIndex(i), hashTags.getIndex(i), image.getIndex(i), url.getIndex(i));
+    productList.enqueue(object);
+}
 
-item2 = new myProjects('Budget Calculator', '#HTML #CSS #JS #Node.js #SQL', './IMG/Budget_Calculator_Project.png', 'https://batking74.github.io/Budget-Calculator/');
+productList.list.forEach(project => displayProjects(project));
 
-item3 = new myProjects('Talking Computer', '#HTML #CSS #JS', './IMG/Talking_Computer_Project.png', 'https://batking74.github.io/Talking_Computer_Project/');
+// Filter Projects
+const filter = document.querySelectorAll('.filter-btn');
+filter.forEach(btn => {
 
-item4 = new myProjects('Digital Art Portfolio', '#HTML #CSS', './IMG/Art_Portfolio_Project.png', 'https://batking74.github.io/Digital_Art_Portfolio_Project--First-ever-project-/');
-
-item5 = new myProjects('Password Generator', '#HTML #CSS #JS', './IMG/Password Generator.png', 'https://batking74.github.io/Password-Generator/');
-item6 = new myProjects('Coming Soon', '', './IMG/Lion2.JPG', '');
-item7 = new myProjects('Coming Soon', '', './IMG/Lion2.JPG', '');
-item8 = new myProjects('Coming Soon', '', './IMG/Lion2.JPG', '');
-myProjectsArray = [item1, item2, item3, item4, item5, item6, item7, item8];
-myProjectsArray.forEach(project => {
-    projectContainer.innerHTML += `
-            <a href="${project.link}" class="project-card" data-tags="#all, ${project.tags}">
-                <img src="${project.image}" alt="">
-                <div class="content">
-                    <h1 class="project-name">${project.name}</h1>
-                    <span class="tags">${project.tags}</span>
-                </div>
-            </a>
-    `;
+    // If any of the buttons are clicked filter the projects based on the skill
+    btn.addEventListener('click', (e) => {
+        if(e.target.classList[1] != 'active') {
+            projectContainer.innerHTML = '';
+            for(let i = 0; i < productList.size(); i++) {
+                const project = productList.getIndex(i);
+                if(project.tags.includes(`#${e.target.id}`)) {
+                    displayProjects(project);
+                }
+                else if(e.target.id === 'all') {
+                    displayProjects(project);
+                }
+            }
+            filter.forEach(ele => ele.classList.remove('active'));
+            btn.classList.add('active');
+        }
+    })
 })
+
+// Displays Products on Web Page
+function displayProjects(project) {
+    projectContainer.innerHTML += `
+        <a href="${project.link}" class="project-card" data-tags="#all, ${project.tags}">
+            <img src="${project.image}" alt="">
+            <div class="content">
+                <h1 class="project-name">${project.name}</h1>
+                <span class="tags">${project.tags}</span>
+            </div>
+        </a>
+    `;
+}
+
+
+// Displays the Number of Projects I got comming soon
+function projectsComingSoon(num) {
+    for(let i = 0; i < num; i++) {
+        title.enqueue(placeHolder[0]);
+        hashTags.enqueue(placeHolder[1]);
+        image.enqueue(placeHolder[2]);
+        url.enqueue(placeHolder[3]);
+    }
+}
 
 // Contact Form
 const form = document.getElementById('form');
@@ -138,6 +245,7 @@ form.addEventListener('submit', (e) => {
     }
 )
 
+// Validates user input
 function validateUserInput() {
     new Promise((resolve, reject) => {
         if(userInfo[0, 1, 2, 3].value == '') {
