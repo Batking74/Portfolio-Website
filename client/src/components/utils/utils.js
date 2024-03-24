416cd6a5033a548395dc87a12266aea7976c06a5
Nazir Knuckles <nazirknuckles@gmail.com>
2024-03-24 15:09:52 -0400

    Added more to sass files

--- client/src/components/utils/utils.js
+++ client/src/components/utils/utils.js
@@ -1,10 +1,8 @@
 export function setActive({ children }, classList) {
 
     // Grabbing each elements classList to see which one was active before, and removing the active class
-    for(let i = 0; i < children.length; i++) {
-        if(children[i].children[0].classList[1] === 'active') {
-            children[i].children[0].classList.remove('active');
-        }
+    for(let c of children) {
+        if(c.children[0].classList[1] === 'active') c.children[0].classList.remove('active');
     }
 
     // Adding the active class to the new btn the user clicked on to filter
